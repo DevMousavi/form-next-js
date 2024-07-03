9cc3852411783c30cd5ac37e13873308c8327e21
DevMousavi <mousavii.dev@gmail.com>
2024-07-03 10:49:59 +0330

    The 'DropDown' component is finished.

--- src/app/create-item/page.tsx
+++ src/app/create-item/page.tsx
@@ -1,7 +1,68 @@
+import Dropdown from "../../components/DropDown";
 import React from "react";
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div className="mobileContainer shadowLayOut  py-9 items-center justify-start flex-col gap-4">
+            <h2 className="font-bold text-primaryColor flex flex-row-reverse gap-2 items-center justify-center border border-solid border-primaryColor px-3 py-2 rounded-md containerTextShadow">
+                <span>***</span>
+                <span>Create</span>
+                <span>Item</span>
+                <span>Form</span>
+                <span>***</span>
+            </h2>
+            <Dropdown
+                url={"/Sale/v1/Category/hidigimenu/List/1"}
+                label={"لیست دسته بندی ها"}
+                dataSend={{
+                    totalCount: 0,
+                    pageIndex: 0,
+                    itemsPerPage: 0,
+                    items: "null",
+                    sortBy: "id",
+                    sortOrder: 0,
+                }}
+            />
+        </div>
+    );
 };
 
 export default page;
+
+// {
+//     "categoriesId": 0,
+//     "name": "string",
+//     "itemCode": "stri",
+//     "description": "string",
+//     "priority": 0,
+//     "parentId": 0,
+//     "preparationTime": 0,
+//     "mealType": 0,
+//     "dailyInventory": 0,
+//     "fixDailyInventory": 0,
+//     "lable": 0,
+//     "displayStatus": 0,
+//     "status": 0,
+//     "price": 0,
+//     "priceAfterDiscount": 0,
+//     "packagingCost": 0,
+//     "taxPercent": 0,
+//     "tags": [
+//       "string"
+//     ],
+//     "itemFiles": [
+//       {
+//         "fileName": "string",
+//         "fileType": 0
+//       }
+//     ],
+//     "weekdays": [
+//       0
+//     ],
+//     "itemPrinters": [
+//       {
+//         "serviceTypeId": 0,
+//         "printerId": 0
+//       }
+//     ]
+//   }
--- src/services/config.js
+++ src/services/config.js
@@ -3,8 +3,52 @@ import axios from "axios";
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     headers: {
+        Accept: "*/*",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZTUwZGY4Mi1iZGU3LTRiYmMtYTQwZS1lMzAyN2FiZjQ0MzEiLCJVc2VySWQiOiIxIiwiRGlzcGxheU5hbWUiOiLYp9iz2YXbjCDZhdit2LPZhiIsIlVzZXJOYW1lIjoiTW9uZW0iLCJFbWFpbCI6Im1vc3RhZmEubW9uZW1Ab3V0bG9vay5jb20iLCJleHAiOjE3MjAwNzI0NDQsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTAwMCIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJ9.GN0k1bkv4DPSFpnUnYfdC26vxbWHMiqqCQjjFcRndu8",
         "Content-Type": "application/json",
     },
 });
+
+// api.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     async (error) => {
+//         const originalRequest = error.config;
+
+//         if (
+//             error.response &&
+//             error.response.status === 409 &&
+//             !originalRequest._retry
+//         ) {
+//             originalRequest._retry = true;
+
+//             // Check if the "refreshToken" exists
+//             const refreshToken = getCookies("token");
+//             // Retrieve a new access token if the "refreshToken" exists
+//             if (refreshToken) {
+//                 try {
+//                     const response = await axios.post(
+//                         `${import.meta.env.VITE_BASE_URL}/auth/refresh-token`,
+//                         { refreshToken: refreshToken }
+//                     );
+//                     // Set the "accessToken" in the cookies for subsequent requests
+//                     if (response.status === 200 || response.status === 201) {
+//                         const newAccessToken = response.data.newAccessToken;
+//                         setCookies("accessToken", newAccessToken);
+//                     }
+//                     // Resend the user's request
+//                     return api(originalRequest);
+//                 } catch (error) {
+//                     console.log(error);
+//                     return error;
+//                 }
+//             }
+//         }
+
+//         return Promise.reject(error);
+//     }
+// );
 
 export { api };
